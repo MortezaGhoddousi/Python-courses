4654b2409870b87ec05840efb704d3e1be209f8e
MortezaGhoddousi <djmgh1994@gmail.com>
2023-09-03 14:27:28 +0430

    Added breaking the text part

--- Python 19/session6.py
+++ Python 19/session6.py
@@ -35,3 +35,19 @@ for letter in encryptedText:
 
 print(decryptedText)
 
+
+## Breaking the text
+for key in range(len(letters)):
+    DecryptedText = []
+    decryptedText = ""
+    for letter in encryptedText:
+        ind = letters.index(letter)
+        if ind-key < 0:
+                ind = ind-key+len(letters)
+        else:
+            ind = ind-key
+        DecryptedText.append(letters[ind])
+        decryptedText = decryptedText+letters[ind]
+
+    print(decryptedText)
+
