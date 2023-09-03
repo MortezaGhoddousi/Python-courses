299d4426e9f97eb4ee0bc3a9d12759f3430b7d8c
MortezaGhoddousi <djmgh1994@gmail.com>
2023-09-03 14:21:16 +0430

    Added Session 6 - Ceasar encryption

---
+++ Python 19/session6.py
@@ -0,0 +1,37 @@
+
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+plainText = "PYTHONISFUN"
+
+key = 15
+
+## Encryption
+EncryptedText = []
+encryptedText = ""
+for letter in plainText:
+    ind = letters.index(letter)
+    if ind+key > len(letters)-1:
+        ind = ind+key-len(letters)
+    else:
+        ind = ind+key
+    EncryptedText.append(letters[ind])
+    encryptedText = encryptedText+letters[ind]
+
+print(encryptedText)
+
+
+## Decryption
+DecryptedText = []
+decryptedText = ""
+for letter in encryptedText:
+    ind = letters.index(letter)
+    if ind-key < 0:
+            ind = ind-key+len(letters)
+    else:
+        ind = ind-key
+    DecryptedText.append(letters[ind])
+    decryptedText = decryptedText+letters[ind]
+
+print(decryptedText)
+
