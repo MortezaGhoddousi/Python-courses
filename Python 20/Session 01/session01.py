521aab6243c6b94ef42efb60a22339d9bf538cc4
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-10-23 16:51:06 +0330

    Added session 1 to Web 15

---
+++ Python 20/Session 01/session01.py
@@ -0,0 +1,15 @@
+# Print Function 
+
+print("Hello World!")
+print('Welcome')
+
+print(5.9)
+
+print("Number", 6)
+
+# Variable and memory
+
+Firstname = "Morteza"
+
+print(Firstname)
+print(type(Firstname))
