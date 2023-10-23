06ae4932b018836645c876314b494af1d7687d3f
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-10-23 16:52:52 +0330

    Added session 2 to Python 20

---
+++ Python 20/Session 02/Session02.py
@@ -0,0 +1,36 @@
+firstName = "Morteza"
+lastName = "Ghoddousi"
+
+age = 29
+secage = "31"
+
+score = 18.94
+
+color = "Blue"
+
+print(firstName, type(firstName))
+print(lastName, type(lastName))
+print(age, type(age))
+print(secage, type(secage))
+print(score, type(score))
+
+print(age+2)
+# print("31"+2)
+# print(secage+2)
+
+humidity = 29
+tempreture = 24
+
+fan = -5
+
+newTempreture = tempreture+fan
+
+print("new Tempreture is: ", newTempreture, "celcius")
+
+jeans = 12
+profit = 2
+
+newPrice = jeans+profit
+
+print(newPrice)
+
