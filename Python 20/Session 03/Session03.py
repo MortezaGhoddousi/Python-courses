05cd4897011cf1055676a98aa3d8ac78aab45281
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-10-25 16:55:41 +0330

    Added session 3 to python 20

---
+++ Python 20/Session 03/Session03.py
@@ -0,0 +1,41 @@
+# Example 1
+
+mid_Score = 19 # taking score for midtermn
+finalScore = 20 # taking score for final
+sumScore = mid_Score+finalScore # calculate sum of them
+average = sumScore/2 # calculate the average score
+
+# display result 
+print("Score of Mid term is:", mid_Score)
+print("Score of Final is:", finalScore)
+print("Sum of your scores is:", sumScore)
+print("Average of your scores is:", average)
+
+# print(average)
+# print(midScore)
+# print(finalScore)
+# print(sumScore)
+
+message = "Hello"
+print(message)
+
+print (5+3)
+
+# print(age)
+
+# Example 2
+
+Mid_score = input("Please enter your mid score: ")
+Mid_score = int(Mid_score)
+print("Your mid score is:", Mid_score)
+print(type(Mid_score))
+
+Final_score = int(input("Please enter your final score: "))
+print("Your final score is:", Final_score)
+print(type(Final_score))
+
+Sum_score = Mid_score+Final_score
+print("Sum of your scores is:", Sum_score)
+
+Average_score = Sum_score/2
+print("Average of your scores is:", Average_score)
