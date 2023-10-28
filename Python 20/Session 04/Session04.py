0de1c521d53399dfe2de1a3216357a8d481a0e1c
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-10-28 16:56:06 +0330

    Added session 4 to Python 20

---
+++ Python 20/Session 04/Session04.py
@@ -0,0 +1,56 @@
+# x = input()
+# print(x)
+
+# print(input())
+
+x = 5/2
+print(type(x))
+
+x = 4/2
+print(x)
+print(type(x))
+
+name = "Morteza"
+print (name*2)
+
+# Data Convertion
+
+x = 2
+print(type(x))
+
+x = str(x)
+print(type(x))
+
+print("The value of x is: " + x)
+
+x = float(x)
+print(type(x))
+
+distance = 14
+unit = "km"
+print(str(distance)+unit)
+print(type(distance))
+
+# Comparison operations
+check = True
+print(type(check))
+
+print(5<9)
+print(100<57)
+
+x = 12
+y = 20
+
+print("Lower than", x < y)
+print("Greater than", x > y)
+print("Lower and equal than", x <= y)
+print("Greater and equal than", x >= y)
+print("Equal", x == y)
+print("Not equal", x != y)
+
+# presence = 
+# temp = 
+# print(presence or temp) # True
+# print(presence and temp) # True
+# print(presence or temp) # False
+# print(presence and temp) # False
