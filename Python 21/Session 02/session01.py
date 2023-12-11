badafcb8191ec06c38b69b3e3e4b956dab6176a6
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-12-11 17:18:17 +0330

    Added session 2 to Python 21

---
+++ Python 21/Session 02/session01.py
@@ -0,0 +1,31 @@
+print('Hello world!')
+
+# Creating a variable to asign my name
+firstname = 'Morteza' # String
+# Calling the variable
+print(firstname)
+print('firstname')
+
+# Types of variable
+lastname = "Ghoddousi"
+age = 29 # Integer
+score = 18.94 # Float
+is_male = True # Boolean
+
+print(type(age))
+print(type(lastname))
+print(type(score))
+print(type(is_male))
+
+print(7+3)
+print(10-5)
+print(3*5)
+print(10/2)
+
+x = 10/2
+
+message = "Debugging"
+
+print(message)
+
+# first_name = 
