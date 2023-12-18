3a2af082949cb92a595178e9cc122dbc1484b941
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-12-18 17:21:46 +0330

    Added session 04 to Python 21

---
+++ Python 21/Session 04/session04.py
@@ -0,0 +1,30 @@
+# x = int(input('Enter the number: '))
+
+# if x%2 == 0:
+#     print('The number is even')
+# else:
+#     print('The number is odd')
+
+
+# names = ['Morteza', 'Hadi', 'Mahdi', 'Honey', 29, 27, 18, 20]
+
+# print(names[1])
+
+# print('Start of the for loop')
+
+# for name in names:
+#     print('hello')
+
+# print('End of the for loop')
+
+x = int(input('Enter the number: '))
+
+i = 0
+
+while i<x:
+    print(f'variable i is: {i}')
+    i = i+1
+
+while i>=0:
+    print(f'variable i is: {i}')
+    i = i-1
