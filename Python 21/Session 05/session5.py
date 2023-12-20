b7a90ef612e4bf17d3a2748677d2476156099b93
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2023-12-20 17:19:28 +0330

    Added session 05 to Python 21

---
+++ Python 21/Session 05/session5.py
@@ -0,0 +1,30 @@
+# # With While Loop
+# x = int(input("Enter a Number :"))
+# i = 0
+# while i < x:
+#     print(i)
+#     i = i + 1
+
+
+# while i >= 0:
+#     print(i)
+#     i = i - 1
+
+
+# # With For Loop
+
+# for a in range(x):
+#     print(a)
+    
+# for i in range(x+1):
+#     print(x-i)
+
+
+
+for i in range(3):
+    x = int(input('Enter the score: '))
+    if x>=10:
+        print('Passed')
+    else:
+        print('failed')
+    
