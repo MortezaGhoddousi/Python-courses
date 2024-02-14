14b9b2208ab1cc93d9e0ec1f6bf3771bf1a9a088
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-02-14 15:34:01 +0330

    Added session 01 to Python 23

---
+++ Python 23/Session 01/main.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print('Hello World!')
+
+firstName = 'Morteza'
+lastName = 'Ghoddousi'
+age = 29
+job = 'developer'
+
+print(firstName)
+print(lastName)
+print(age)
+print(job)
+
