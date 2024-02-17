1711e7f57fa969c4a276861550a7880340165ec6
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-02-17 15:38:04 +0330

    Added session 02 to Python 23

---
+++ Python 23/Session 02/main.py
@@ -0,0 +1,25 @@
+print('Morteza')
+
+fname = 'Morteza'
+lname = 'Ghoddousi'
+age = 29
+
+print(age)
+
+# String data
+text = "Welcome to the python class!"
+print(type(text))
+
+# Integer data
+age = 29
+print(type(age))
+
+# Float data
+temperature = 27.4
+print(type(temperature))
+
+role = Analyst
+
+
+
+
