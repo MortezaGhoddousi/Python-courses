d881d88634451100263f1d00080fd25609ff287b
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-02-19 15:22:13 +0330

    Added session 03 to Python 23

---
+++ Python 23/Session 03/main.py
@@ -0,0 +1,47 @@
+# print('Hello world!')
+
+# midTerm = 17
+# final = 19
+
+# sumScores = midTerm+final
+# print(sumScores)
+
+# x = input()
+
+# print(x)
+
+# y = input('Enter your score: ')
+# print(y)
+# print(type(y))
+
+
+# fname = input('Enter your first name: ')
+
+
+# midTerm = input('Enter your midterm score: ')
+# final = input('Enter your final score: ')
+
+# midTerm = float(midTerm)
+# final = float(final)
+
+# sumScores = midTerm+final
+
+# print(sumScores)
+
+# fname = 'Morteza'
+# lname = 'Ghoddousi'
+
+# print(fname + lname)
+
+# x = 9
+
+# y = 3
+
+# print(x/y)
+
+score = 19.5
+score = str(score)
+
+student = 'Morteza'
+
+print('The score of ' + student + ' is: ' + score)
