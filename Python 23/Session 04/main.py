67590089f36d35a512e3d60661629d4aedb0d5c1
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-02-26 17:10:20 +0330

    Added session 04 to Python 23

---
+++ Python 23/Session 04/main.py
@@ -0,0 +1,45 @@
+x = 10 #Integer
+y = 17.43 #Float
+z = 'Hello world!' #String
+
+x1 = float(x)
+print(x1)
+print(type(x1))
+
+y1 = int(y)
+print(y1)
+print(type(y1))
+
+x2 = str(x)
+print(z+x2)
+print(type(x2))
+
+print(int('10'))
+# print(int('morteza'))
+
+# score = input('Enter your score: ')
+# print(type(score))
+# score = float(score)
+# print(type(score))
+
+# age = int(input('Enter your age: '))
+# print(type(age))
+
+print(5 < 9)
+print(5 <= 9)
+print(5 > 9)
+print(5 >= 9)
+print(5 == 9)
+print(5 != 9)
+
+
+print(f'{True} and {True} is: ', True and True)
+print(f'{True} and {False} is: ', True and False)
+print(f'{False} and {True} is: ', False and True)
+print(f'{False} and {False} is: ', False and False)
+
+print(f'{True} or {True} is: ', True or True)
+print(f'{True} or {False} is: ', True or False)
+print(f'{False} or {True} is: ', False or True)
+print(f'{False} or {False} is: ', False or False)
+
