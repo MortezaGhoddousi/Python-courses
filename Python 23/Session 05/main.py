e2c2a9e56b5d1b750078d772728910a8c4f406a1
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-03-02 15:29:19 +0330

    Added session 05 to Python 23

---
+++ Python 23/Session 05/main.py
@@ -0,0 +1,68 @@
+# lists 
+
+name1 = 'Morteza'
+name2 = 'Shayan'
+
+print(name1)
+
+names = ['Morteza', name2, 29, True, 16.78]
+
+print(names[1])
+
+
+print('hello')
+print('hello')
+print('hello')
+
+
+
+# For loops 
+
+print('=============================================')
+
+l = ['hello', 'hello', 'hello']
+
+for i in l:
+    print(i)
+    
+print('end')
+
+print('=============================================')
+
+l = [0, 1, 2]
+
+for i in l:
+    print('hello')
+    
+print('end')
+
+print('=============================================')
+
+l = range(3)
+
+for i in l:
+    print('hello')
+    
+print('end')
+
+print('=============================================')
+
+for i in range(3):
+    print(i)
+    print('hello')
+
+print('end')
+
+print('=============================================')
+
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
