b95a0bbd84b2897e78d5dac2c3ef430a83b368c1
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-03-05 15:19:56 +0330

    Added session 02 to Python 25

---
+++ Python 25/Session 02/main.py
@@ -0,0 +1,25 @@
+# String 
+
+firstname = 'Morteza'
+lastname = "Ghoddousi"
+
+print('Hello!')
+print(firstname)
+print(lastname)
+
+# Integer
+
+age = 29
+
+# Float
+
+height = 185.6
+
+print(7 + 3.0)
+print(10 - 5)
+print(5 * 3)
+print(10 / 2)
+
+price = 99
+price = price + 100
+print(price)
