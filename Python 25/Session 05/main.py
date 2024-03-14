7e23b89d37a53f73e3e69996fb47368359ad906c
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-03-14 17:18:39 +0330

    Added session 25 to Python 25

---
+++ Python 25/Session 05/main.py
@@ -0,0 +1,36 @@
+print('hello')
+print('hello')
+print('hello')
+
+# List
+
+fname = 'Morteza'
+lname = 'Ghoddousi'
+age = 29
+weight = 87
+height = 1.86
+isMale = True
+
+Morteza = ['Morteza', lname, age, weight, height, isMale]
+print(Morteza)
+print(lname)
+print(Morteza[1])
+print(Morteza[4])
+
+# For loops
+
+# for (variable) in (list):
+
+for i in Morteza:
+    print(i)
+
+print('end')
+
+# hello = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# hello = range(10)
+
+for i in range(10):
+    print(i)
+    print('hello')
+
+
