c7684cd895ec17c0d35589f633b52fdf287739ea
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-08 15:16:53 +0330

    Added session 09 to Python 23

---
+++ Python 23/Session 09/main.py
@@ -0,0 +1,35 @@
+age = 30
+if age > 18:
+    print('Adult')
+
+i = 0
+while i<5:
+    print(i)
+    i = i+1
+
+for i in range(5):
+    print(i)
+    
+print('The end')
+
+print("=====================================")
+
+# admin = '123'
+
+# guess = input('Enter your password: ')
+
+# while not admin == guess:
+#     print('Wrong password!')
+#     guess = input('Enter your password again: ')
+    
+# print(admin, guess)
+
+# print('Access Granted!')
+
+print("=====================================")
+
+age = 20
+if age < 18: 
+    print("Apply Discount")
+    
+print("Proceed to Payment")
