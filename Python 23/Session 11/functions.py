fe9f2096009c4edccd749693e634da3093d93481
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-21 13:24:47 +0330

    Added session 11 to Python 23

---
+++ Python 23/Session 11/functions.py
@@ -0,0 +1,3 @@
+def f(x):
+    y = 2*x+3
+    return y
---
+++ Python 23/Session 11/main.py
@@ -0,0 +1,10 @@
+import functions
+import numpy
+
+x = 3
+y1 = functions.f(x)
+print(y1)
+y2 = functions.f(7)
+print(y2)
+y3 = functions.f(5)
+print(y3)
