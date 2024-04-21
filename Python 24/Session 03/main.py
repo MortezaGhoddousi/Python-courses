4e53cfc26912ab0aca0aa8fb79bd63b27359efe1
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-21 18:06:00 +0330

    Added session 03 to Python 24

---
+++ Python 24/Session 03/main.py
@@ -0,0 +1,31 @@
+print("Hello world!")
+
+## String
+fname = 'Morteza'
+# age = '30'
+
+print(fname)
+
+## Integer
+
+age = 30
+
+## Float
+
+height = 1.86
+
+## Boolean
+
+isMale = True
+
+print(5/2)
+
+print(5//2)
+
+import numpy as np
+
+print(np.mod(5, 2))
+
+
+print(2**3)
+
