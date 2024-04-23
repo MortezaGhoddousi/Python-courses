b1417ffc5379cae4e75f2e1159f15cfa3450e5c8
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-23 18:18:05 +0330

    Added session 04 to Python 24

---
+++ Python 24/Session 04/main.py
@@ -0,0 +1,38 @@
+fname = "Morteza"
+revenue = 300
+print(revenue)
+
+lastName = 'Ghoddousi'
+
+age = input("Enter your age: ")
+print(age)
+print(type(age))
+
+age = int(age)
+print(age)
+print(type(age))
+
+age = age+1
+print(age)
+
+
+weight = int(input('Enter your weight: '))
+print(type(weight))
+
+print(f'Your weight is: {weight}')
+
+
+height = float(input('Enter your height: '))
+print(type(height))
+
+print(f'Your height is: {height}')
+
+print("Morteza " + "Ghoddousi")
+
+print('Your height is: ' + str(height))
+
+
+print(type(5/2))
+print(type(4/2))
+print(4/2)
+
