0d6485539a88d45799e30f4cc98bc3a5cf664c6e
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-04-23 13:20:49 +0330

    Added session 12 to Python 23

---
+++ Python 23/Session 12/functions.py
@@ -0,0 +1,3 @@
+def addNumbers(x, y):
+    z = x+y
+    return z
---
+++ Python 23/Session 12/main.py
@@ -0,0 +1,32 @@
+fname = 'Morteza'
+
+print(fname)
+
+print(fname.upper())
+print(fname.lower())
+
+print(fname.find('r'))
+print(fname.find('h'))
+
+mylist = ['Morteza', 'Shayan']
+
+mylist.append('Hassan')
+mylist.insert(1, 'Ali')
+mylist.insert(3, 'Omid')
+
+print(len(mylist))
+print(mylist)
+
+mylist.pop(1)
+
+print(len(mylist))
+print(mylist)
+
+import functions
+
+m = functions.addNumbers(4, 6)
+print(m)
+
+
+m = functions.addNumbers(3, 6)
+print(m)
