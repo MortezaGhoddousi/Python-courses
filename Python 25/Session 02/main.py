4325cdf955abaf46b3fd7f1cb7a20b5465ba8d5c
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-07-04 19:47:46 +0330

    Added session 02 to Python 25

---
+++ Python 25/Session 02/main.py
@@ -0,0 +1,33 @@
+print('Hello world!')
+
+fname = "Morteza"
+
+print(fname)
+print(type(fname))
+
+age = 30
+
+print(age)
+print(type(age))
+
+height = 1.86
+
+print(height)
+print(type(height))
+
+x = 5
+y = 7
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+fname = 'Morteza'
+lname = 'Ghoddousi'
+age = 30
+
+print(fname + lname)
+print(fname*3)
+print(age*3)
