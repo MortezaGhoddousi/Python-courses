b57e1644044791a97a0020af3ae4a7daca2bc705
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-07-11 20:03:19 +0330

    Added session 03 to Python 25

---
+++ Python 25/Session 03/main.py
@@ -0,0 +1,39 @@
+# Define some variable 
+fname = 'Morteza'
+lname = "ghoddousi"
+age = 30
+
+print(fname+lname)
+
+x = 4
+y = 8
+print(x+y)
+
+
+
+# Printing the variables
+print('Your first name: ' + fname)
+# print(lname)
+print('Your age: ' + str(age))
+print(f'Your age: {age}')
+
+# print('Please enter your password: ')
+password = input('Please enter your password: ')
+print('Your password is: ' + password)
+print(type(password))
+
+password = int(password)
+print(type(password))
+
+print(int(password) * 2)
+print(int(password) + 2)
+
+height = input('Please enter your height: ')
+print(type(height))
+height = float(height)
+print(type(height))
+
+print(height)
+
+print(2+3.0)
+print(type(2+3.0))
