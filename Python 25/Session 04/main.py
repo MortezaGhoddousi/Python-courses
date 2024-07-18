4f73c734f8b20ced4f3b86fd45090d23604c5587
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-07-18 19:53:01 +0330

    Added session 04 to Python 25

---
+++ Python 25/Session 04/main.py
@@ -0,0 +1,39 @@
+# Comparison Operators
+
+print("Comparison Operators")
+x = 4
+y = 5
+
+print(f'{x} < {y} = {x<y}')
+print(f'{x} <= {y} = {x<=y}')
+print(f'{x} > {y} = {x>y}')
+print(f'{x} >= {y} = {x>=y}')
+print(f'{x} == {y} = {x==y}')
+print(f'{x} != {y} = {x!=y}')
+
+# Logical Operators
+
+print("Logical Operators")
+
+print(f'{True} and {True} = {True and True}')
+print(f'{True} and {False} = {True and False}')
+print(f'{False} and {True} = {False and True}')
+print(f'{False} and {False} = {False and False}')
+
+
+print(f'{True} or {True} = {True or True}')
+print(f'{True} or {False} = {True or False}')
+print(f'{False} or {True} = {False or True}')
+print(f'{False} or {False} = {False or False}')
+
+print(f'not {False} = {not False}')
+print(f'not {True} = {not True}')
+
+# Example
+
+print((4<9) and (8!=4) or True)
+
+print('hello')
+
+
+
