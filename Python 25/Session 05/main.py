8e97b7bff168a2dfd5f45a8bbfa6ba8f4dc1b2c9
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-07-21 19:59:18 +0330

    Added session 05 to Python 25

---
+++ Python 25/Session 05/main.py
@@ -0,0 +1,54 @@
+i = 0
+
+while i<5:
+    print(i)
+    i = i+1
+    print('hello')
+
+print('end')
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+numbers1 = range(20)
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+print(numbers[7])
+print(numbers[8])
+print(numbers[9])
+print(numbers[10])
+print(numbers[11])
+print(numbers[12])
+print(numbers[13])
+print(numbers[14])
+print(numbers[15])
+print(numbers[16])
+print(numbers[17])
+print(numbers[18])
+print(numbers[19])
+
+i = 0
+while i<20:
+    print(numbers1[i])
+    i = i+1
+    
+for i in range(100):
+    print('hello')
+    
+
+age = 18
+if age > 18:
+    print('adult')
+elif age > 10 and age <= 18:
+    print('teenager')
+else: 
+    print('child')
+ 
+
+
+
