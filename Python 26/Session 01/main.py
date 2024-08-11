929696cc1fb1a0bd4b445e9cdeab96db435d6c8c
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-11 16:54:51 +0330

    Added session 01 to Python 26

---
+++ Python 26/Session 01/main.py
@@ -0,0 +1,22 @@
+print("Hello world!")
+
+# Strings
+firstName = "Morteza" 
+lastName = 'Ghoddousi'
+
+# Integers
+age = 30
+weight = 92
+
+# Floats
+height = 1.86
+
+# Booleans
+isMale = True
+
+print(firstName)
+print(lastName)
+print(age)
+print(height)
+print(weight)
+print(isMale)
