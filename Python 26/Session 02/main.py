78754b9518ecbaa770ffd59c97233cb2582f07b4
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-13 16:48:04 +0330

    Added session 02 to Python 26

---
+++ Python 26/Session 02/main.py
@@ -0,0 +1,40 @@
+x = 4
+y = 1
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+print(x)
+x = 2
+print(x)
+
+y = y+5
+
+a = '2'
+b = "3"
+
+print(a + b)
+
+# print("enter your age: ")
+# age = input("enter your age: ")
+
+# print(age)
+
+midTermScore = input("enter your midterm score: ")
+finalScore = input("enter your final score: ")
+
+print(type(midTermScore))
+
+midTermScore = float(midTermScore)
+
+print(type(midTermScore))
+
+
+finalScore = int(finalScore)
+
+
+print(midTermScore + finalScore)
+
