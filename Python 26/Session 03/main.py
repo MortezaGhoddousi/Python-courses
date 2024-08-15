6d2a21f115deb09dbf0b7f27ed051cd2c11eb15f
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-15 18:21:02 +0330

    added session 03 to Python 26

---
+++ Python 26/Session 03/main.py
@@ -0,0 +1,32 @@
+print("Comparison Operators")
+
+x = 4
+y = 7
+print(f"{x} < {y} = {x < y}")
+print(f"{x} <= {y} = {x <= y}")
+print(f"{x} > {y} = {x > y}")
+print(f"{x} >= {y} = {x >= y}")
+print(f"{x} == {y} = {x == y}")
+print(f"{x} != {y} = {x != y}")
+
+print("Logical Operators")
+
+print(f"True and True = {True and True}")
+print(f"True and False = {True and False}")
+print(f"False and True = {False and True}")
+print(f"False and False = {False and False}")
+
+print(f"True or True = {True or True}")
+print(f"True or False = {True or False}")
+print(f"False or True = {False or True}")
+print(f"False or False = {False or False}")
+
+print(f"not True = {not True}")
+print(f"not False = {not False}")
+
+print(((4<3) or (3!=2)) and (((5==5) and False) or True))
+
+x = 30
+
+print("The value of x: " + str(x) + " and final score is 40")
+print(f"The value of x: {x} and final score is 40")
