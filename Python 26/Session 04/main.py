6701f3b83ca3777cf2beaed8ab9e8bd0a23b0f5f
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-18 18:40:01 +0330

    Added session 04 to Python 26

---
+++ Python 26/Session 04/main.py
@@ -0,0 +1,22 @@
+# x = int(input('enter the first number: '))
+# y = int(input('enter the second number: '))
+
+# average = (x + y) / 2
+
+# print(average)
+
+# if x>y:
+#     print(f"{x} is greater than {y}")
+# else:
+#     print(f"{y} is greater than {x}")
+    
+# print('end')
+    
+age = int(input('enter your age: '))
+
+if age >= 18:
+    print("Adult")
+elif age >= 10 and age < 18:
+    print("Teenager")
+else:
+    print("Child")
