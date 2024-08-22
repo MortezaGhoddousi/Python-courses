2571b079dcf850a05df0fd5cb214267b7e11e024
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-08-22 19:48:44 +0330

    Added session 12 to Python 25

---
+++ Python 25/Session 12/main.py
@@ -0,0 +1,26 @@
+# import numpy
+
+# def Roots(a, b, c):
+#     delta = b**2 - 4*a*c
+#     if delta > 0:
+#         x1 = (-b + numpy.sqrt(delta))/(2*a)
+#         x2 = (-b - numpy.sqrt(delta))/(2*a)
+#         return [x1, x2]
+#     elif delta == 0:
+#         return -b/(2*a)
+#     else:
+#         return "There are no roots for this polynomial"
+    
+# print(Roots(1, 3, -4))
+        
+        
+# def factorial(x):
+#     s = 1
+#     for i in range(1, x+1):
+#         s = s*i
+#     return s
+
+# print(factorial(10))
+        
+
+        
