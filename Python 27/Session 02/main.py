5f77daa6463b43e9ce9a9ec7383eb79017e10e12
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-09-23 19:43:31 +0330

    Added session 02 to Python 27

---
+++ Python 27/Session 02/main.py
@@ -0,0 +1,21 @@
+firstName = 'Morteza'
+lastName = "Ghoddousi"
+age = 30
+weight = 92
+height = 1.86
+isMale = True
+
+print(firstName, lastName)
+print(age)
+print(weight, 'Kg')
+print(height, 'm')
+print(isMale)
+
+
+x = 8
+y = 3
+
+print(x / y)
+
+print(x // y)
+print(x % y)
