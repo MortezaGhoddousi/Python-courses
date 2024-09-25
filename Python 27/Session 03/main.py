5a0804ee6c05b772f1906b64f913868cf232ffca
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-09-25 19:34:47 +0330

    Added session 03 to Python 27

---
+++ Python 27/Session 03/main.py
@@ -0,0 +1,67 @@
+# message = "Debugging"
+# print(message)
+
+# revenue = 300
+# print(revenue)
+
+# print(5)
+
+# print(4, 10, 'salam', True)
+
+# print('Enter your name: ')
+# fname = input("Enter your name: ")
+# print('Your name is: ', fname)
+
+# x = input('Enter your first number: ')
+# x = float(x)
+
+# y = int(input('Enter your second number: '))
+
+
+# print(type(x))
+
+# Sum = x + y
+
+# print(type(Sum), Sum)
+
+# avg = (x+y)/2
+
+# print("Summation of your numbers: ", Sum)
+# print("Average of your numbers: ", avg)
+
+# distance = 14
+# unit = 'km'
+
+# distance = str(distance)
+
+# print(distance + unit)
+
+
+# # Comparison Operators
+x = 5
+y = 19.5
+
+print(f"{x} < {y} =>", x<y)
+print(f"{x} <= {y} =>", x<=y)
+print(f"{x} > {y} =>", x>y)
+print(f"{x} >= {y} =>", x>=y)
+print(f"{x} == {y} =>", x==y)
+print(f"{x} != {y} =>", x!=y)
+
+
+# # Logical Operators
+
+print(f"{True} and {True} =>" , True and True)
+print(f"{True} and {False} =>" , True and False)
+print(f"{False} and {True} =>" , False and True)
+print(f"{False} and {False} =>" , False and False)
+
+print(f"{True} or {True} =>" , True or True)
+print(f"{True} or {False} =>" , True or False)
+print(f"{False} or {True} =>" , False or True)
+print(f"{False} or {False} =>" , False or False)
+
+print(f"not {True} =>", not True)
+print(f"not {False} =>", not False)
+
+print((4<5) and (4==5) or not True)
