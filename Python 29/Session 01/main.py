894f38715a9984020cc34248eea525b0bb76e647
MortezaGhoddousi <djmgh1994@gmail.com>
2025-08-11 17:27:02 +0330

    added session 01 to Python 29

---
+++ Python 29/Session 01/main.py
@@ -0,0 +1,31 @@
+## Variables
+# String
+firstName = "Morteza"
+
+name = "Amir"
+lastName = "Ali"
+
+# Integer
+age = 31
+weight = 83
+
+number = 32
+Class = 103
+
+# Float 
+height = 1.86
+
+amir = 16.3
+home = 18.4
+
+# Boolean
+isMale = True
+
+
+## Functions
+# Print Function
+print("Morteza")
+print(firstName)
+
+print(name)
+
